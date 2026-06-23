c3d4134ad94b33a63a2db13fbf6168f60f878567
Anshuman Varma <anshumanverma9795@gmail.com>
2026-06-23 20:42:28 +0530

    Updated Splash_Screen to use Thread logic as requested

--- app/src/main/java/com/example/summer_training/Splash_Screen.kt
+++ app/src/main/java/com/example/summer_training/Splash_Screen.kt
@@ -2,19 +2,21 @@ package com.example.summer_training
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class Splash_Screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
-        Handler(Looper.getMainLooper()).postDelayed({
+        Thread {
+            try {
+                Thread.sleep(3000)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
             val intent = Intent(this@Splash_Screen, Login_Screen::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }.start()
     }
 }
